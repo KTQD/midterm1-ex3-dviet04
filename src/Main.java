23da3b2ff9d8bf477fdd9658d39dd4b8b3eaf731
dviet04 <121754515+dviet04@users.noreply.github.com>
2024-03-14 08:46:19 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String in = "Input.txt";
+        String out = "Output.txt";
 
+        try {
+            BufferedReader read = new BufferedReader(new FileReader(in));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = read.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            read.close();
 
-        Bài làm viết tại đây
+            String contentOut = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
+            BufferedWriter write = new BufferedWriter(new FileWriter(out));
+            write.write(contentOut);
+            write.close();
 
+        } catch (IOException e) {
+            System.out.println("Loi");
+        }
     }
 }
